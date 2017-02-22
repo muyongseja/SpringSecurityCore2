b9b1ece5d0c5bb0ca173df856dc989b976f955fa
Jeong Woo <netsong7@naver.com>
2017-02-22 13:12:56 +0900

    test

--- src/main/java/com/netsong7/core2/CustomAuthenticationProvider.java
+++ src/main/java/com/netsong7/core2/CustomAuthenticationProvider.java
@@ -1,11 +1,16 @@
 package com.netsong7.core2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 public class CustomAuthenticationProvider implements AuthenticationProvider{
 	private static final Logger logger = 
@@ -25,7 +30,20 @@ public class CustomAuthenticationProvider implements AuthenticationProvider{
 		
 		logger.info("Welcome authenticate! {}", _id + "/ " + _pw);
 		
-		return null;
+		if(_id.equals("user1") && _pw.equals("1111")){
+			List<GrantedAuthority> roles = new ArrayList<>();
+			roles.add(new SimpleGrantedAuthority("ROLE_USER"));
+			
+			UsernamePasswordAuthenticationToken result = 
+				new UsernamePasswordAuthenticationToken(_id, _pw, roles);
+			
+			//result.setDetails(details);
+			
+			return result;
+		}
+		else{
+			return null;
+		}
 	}
 }
 
