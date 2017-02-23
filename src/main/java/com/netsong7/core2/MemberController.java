b8972700afb04b0537ea2f249770c9813ea5b4a1
Jeong Woo <netsong7@naver.com>
2017-02-23 15:36:42 +0900

    test

--- src/main/java/com/netsong7/core2/MemberController.java
+++ src/main/java/com/netsong7/core2/MemberController.java
@@ -1,10 +1,17 @@
 package com.netsong7.core2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,20 +25,27 @@ public class MemberController {
 	private MemberDao memberDao;
 	
 	@RequestMapping("/user/login")
-	public void login(){
-	
+	public void login(Member member){
+		List<GrantedAuthority> roles = new ArrayList<>();
+		roles.add(new SimpleGrantedAuthority("ROLE_USER"));
+		UsernamePasswordAuthenticationToken token = 
+			new UsernamePasswordAuthenticationToken(member.getUserid(), member.getUserpw());
+		Authentication auth = token;
+		SecurityContextHolder.getContext().setAuthentication(auth);
 	}
 	
 	@RequestMapping("/user/login_success")
 	public void login_success(HttpSession session){
+		/*
 		CustomUserDetails details =
 			(CustomUserDetails)SecurityContextHolder.getContext()
 								.getAuthentication().getDetails();
 		
 		logger.info("Welcome login_success! {}, {}", 
 				details.getUsername(), details.getPassword());
+		*/
 		
-		session.setAttribute("userLogInfo", details);
+		//session.setAttribute("userLogInfo", details);
 	}
 	
 	@RequestMapping("/user/login_duplicate")
--- src/main/java/com/netsong7/core2/MemberDaoImpl.java
+++ src/main/java/com/netsong7/core2/MemberDaoImpl.java
@@ -1,7 +1,11 @@
 package com.netsong7.core2;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Repository;
 
@@ -24,6 +28,20 @@ public class MemberDaoImpl implements MemberDao {
 	public void insertMember(Member member) {
 		member.setUserpw(bcrypt.encode(member.getUserpw()));
 		sqlSession.insert(NAMESPACE + ".insertMember", member);
+		
+		Object[] obj = SecurityContextHolder.getContext()
+				.getAuthentication().getAuthorities().toArray();
+		// 권한 확인
+		/*
+		for(int i=0; i<obj.length; i++){
+			System.out.println(obj[i]);
+		}
+		*/
+		
+		Map map = new HashMap();
+		map.put("userid", member.getUserid());
+		map.put("authority", obj[0].toString());
+		sqlSession.insert(NAMESPACE + ".insertAuth", map); 
 	}
 }
 
